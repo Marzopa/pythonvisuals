2d500efb6fbe2120d7336394d131e7821bb3f49e
oscar <ojimenezrodriguez@ithaca.edu>
2024-09-04 19:19:27 -0400

    made separate function for drawing edges and added type declaration (aesthetic changes)

--- loteria.py
+++ loteria.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw
 
 
-def calc_dimensions(rows: int, cols: int, size: tuple, thickness: int = 20):
+def calc_dimensions(rows: int, cols: int, size: tuple[int, int], thickness: int = 20) -> tuple[int, int]:
     """
     :param rows: number of rows in the loteria
     :param cols: number of columns in the loteria
@@ -12,25 +12,17 @@ def calc_dimensions(rows: int, cols: int, size: tuple, thickness: int = 20):
     return size[0] * cols + (cols + 1) * thickness, size[1] * rows + (rows + 1) * thickness
 
 
-def draw_loteria(rows: int, cols: int, images: list[Image], thickness: int = 20):
+def draw_edges(draw: ImageDraw, dimensions: tuple[int, int], img_size: tuple[int, int],
+               rows: int, cols: int, thickness: int = 20) -> None:
     """
-    :param rows: number of rows in loteria
-    :param cols: number of columns in loteria
-    :param images: a list of image objects to draw loteria from, assumes all image dimensions are the same
+    :param draw: the draw object for a canvas
+    :param dimensions: a tuple (width, height) for the entire, final image
+    :param img_size: a tuple (width, height) for the size of each image
+    :param rows: number of image rows
+    :param cols: number of image columns
     :param thickness: thickness of lines drawn on the loteria (default 20px)
-    :return: an image of the loteria
-    :raises: ValueError if number of images isn't the same as the number of squares in loteria
     """
-    if rows * cols != len(images):
-        raise ValueError("Incorrect number of images")
-
-    img_size: tuple = images[0].size
-    dimensions = calc_dimensions(rows, cols, img_size, thickness)
-    canvas = Image.new('RGB', dimensions, 'black')
-    draw = ImageDraw.Draw(canvas)
-
     thick_adjustment = thickness * 2  # This is so thickness of borders is respected
-
     # This section creates the top and left edges
     draw.line((0, 0, dimensions[0], 0), fill='white', width=thick_adjustment)
     draw.line((0, 0, 0, dimensions[1]), fill='white', width=thick_adjustment)
@@ -41,13 +33,41 @@ def draw_loteria(rows: int, cols: int, images: list[Image], thickness: int = 20)
     # Draw inner column edges
     for c in range(1, cols):
         y = dimensions[1]
-        draw.line((c*(thickness+img_size[0]), 0, c*(thickness+img_size[0]), y), fill='white', width=thickness)
+        draw.line((c * (thickness + img_size[0]), 0, c * (thickness + img_size[0]), y), fill='white', width=thickness)
 
     # Draw inner row edges
     for r in range(1, rows):
         x = dimensions[0]
-        draw.line((0, r*(thickness+img_size[1]), x, r*(thickness+img_size[1])), fill='white', width=thickness)
+        draw.line((0, r * (thickness + img_size[1]), x, r * (thickness + img_size[1])), fill='white', width=thickness)
 
-    for image in images:
-        canvas.paste(image)
+
+def draw_loteria(rows: int, cols: int, images: list[Image], thickness: int = 20) -> Image:
+    """
+    :param rows: number of rows in loteria
+    :param cols: number of columns in loteria
+    :param images: a list of image objects to draw loteria from, assumes all image dimensions are the same
+    :param thickness: thickness of lines drawn on the loteria (default 20px)
+    :return: an image of the loteria
+    :raises: ValueError if number of images isn't the same as the number of squares in loteria
+    :return: an image (canvas) of the loteria
+    """
+    if rows * cols != len(images):
+        raise ValueError("Incorrect number of images")
+
+    img_size: tuple[int, int] = images[0].size
+    dimensions: tuple[int, int] = calc_dimensions(rows, cols, img_size, thickness)
+    canvas = Image.new('RGB', dimensions, 'black')
+    draw = ImageDraw.Draw(canvas)
+
+    draw_edges(draw, dimensions, img_size, rows, cols, thickness)
+
+    # Place images in grid place
+    row_place = 0
+    col_place = 0
+    for image_index in range(len(images)):
+        if col_place % rows == 0:
+            row_place += 1
+            col_place = 0
+        canvas.paste(images[image_index], (col_place*(image_index+thickness), row_place*(image_index+thickness)))
+        col_place += 1
     return canvas
